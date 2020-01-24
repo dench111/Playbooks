00418082559645d02b66fc70d6bd7f11246e58b1
dench111 <46183310+dench111@users.noreply.github.com>
2020-01-25 00:04:44 +0300

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,7 +9,7 @@ pipeline {
       steps {
         script {
           def workspace = "/var/lib/jenkins/workspace/InstallDocker"
-          sh "chmod ugo+rwx $workspace/*"
+          sh "chmod -R ugo+rwx $workspace/"
           sh "ansible -m setup servers > $workspace/Ansible_env_variables.txt"
           sh "cat $workspace/Ansible_env_variables.txt"
           sh "ansible-playbook -i " + "inventory" + " " + "$workspace/Docker/install-docker.yaml"
